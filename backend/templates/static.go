361df54d8bfc79fbe2f3aafc092627007c7c5e0d
hy2015 <seaorc409891192@qq.com>
2017-12-05 23:43:32 +0800

    Add confirm upload dialog

--- backend/templates/static.go
+++ backend/templates/static.go
@@ -6,34 +6,34 @@ const BIND = `<!DOCTYPE html>
     <meta charset="UTF-8">
     <title>绑定企业</title>
     <meta content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0" name="viewport"/>
-	<link rel="stylesheet" href="/html/weui.css">
-	<style>@-moz-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@-webkit-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@-o-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}embed,object{animation-duration:.001s;-ms-animation-duration:.001s;-moz-animation-duration:.001s;-webkit-animation-duration:.001s;-o-animation-duration:.001s;animation-name:nodeInserted;-ms-animation-name:nodeInserted;-moz-animation-name:nodeInserted;-webkit-animation-name:nodeInserted;-o-animation-name:nodeInserted;}</style>
-	<style>@-moz-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@-webkit-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@-o-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}embed,object{animation-duration:.001s;-ms-animation-duration:.001s;-moz-animation-duration:.001s;-webkit-animation-duration:.001s;-o-animation-duration:.001s;animation-name:nodeInserted;-ms-animation-name:nodeInserted;-moz-animation-name:nodeInserted;-webkit-animation-name:nodeInserted;-o-animation-name:nodeInserted;}</style>
-	<style>@-moz-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@-webkit-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@-o-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}embed,object{animation-duration:.001s;-ms-animation-duration:.001s;-moz-animation-duration:.001s;-webkit-animation-duration:.001s;-o-animation-duration:.001s;animation-name:nodeInserted;-ms-animation-name:nodeInserted;-moz-animation-name:nodeInserted;-webkit-animation-name:nodeInserted;-o-animation-name:nodeInserted;}</style>
-	<style>@-moz-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@-webkit-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@-o-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}embed,object{animation-duration:.001s;-ms-animation-duration:.001s;-moz-animation-duration:.001s;-webkit-animation-duration:.001s;-o-animation-duration:.001s;animation-name:nodeInserted;-ms-animation-name:nodeInserted;-moz-animation-name:nodeInserted;-webkit-animation-name:nodeInserted;-o-animation-name:nodeInserted;}</style>
+    <link rel="stylesheet" href="/html/weui.css">
+    <style>@-moz-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@-webkit-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@-o-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}embed,object{animation-duration:.001s;-ms-animation-duration:.001s;-moz-animation-duration:.001s;-webkit-animation-duration:.001s;-o-animation-duration:.001s;animation-name:nodeInserted;-ms-animation-name:nodeInserted;-moz-animation-name:nodeInserted;-webkit-animation-name:nodeInserted;-o-animation-name:nodeInserted;}</style>
+    <style>@-moz-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@-webkit-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@-o-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}embed,object{animation-duration:.001s;-ms-animation-duration:.001s;-moz-animation-duration:.001s;-webkit-animation-duration:.001s;-o-animation-duration:.001s;animation-name:nodeInserted;-ms-animation-name:nodeInserted;-moz-animation-name:nodeInserted;-webkit-animation-name:nodeInserted;-o-animation-name:nodeInserted;}</style>
+    <style>@-moz-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@-webkit-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@-o-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}embed,object{animation-duration:.001s;-ms-animation-duration:.001s;-moz-animation-duration:.001s;-webkit-animation-duration:.001s;-o-animation-duration:.001s;animation-name:nodeInserted;-ms-animation-name:nodeInserted;-moz-animation-name:nodeInserted;-webkit-animation-name:nodeInserted;-o-animation-name:nodeInserted;}</style>
+    <style>@-moz-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@-webkit-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@-o-keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}@keyframes nodeInserted{from{opacity:0.99;}to{opacity:1;}}embed,object{animation-duration:.001s;-ms-animation-duration:.001s;-moz-animation-duration:.001s;-webkit-animation-duration:.001s;-o-animation-duration:.001s;animation-name:nodeInserted;-ms-animation-name:nodeInserted;-moz-animation-name:nodeInserted;-webkit-animation-name:nodeInserted;-o-animation-name:nodeInserted;}</style>
 </head>
 <body>
-	<div class="weui-cells weui-cells_form">
+    <div class="weui-cells weui-cells_form">
             <div class="weui-cell">
                 <div class="weui-cell__hd"><label class="weui-label">手机号</label></div>
                 <div class="weui-cell__bd">
                     <input id="phone" class="weui-input" type="number" pattern="[0-9]*" placeholder="请输入手机号">
                 </div>
             </div>
-			<div class="weui-cell">
+            <div class="weui-cell">
                 <div class="weui-cell__hd"><label class="weui-label">密码</label></div>
                 <div class="weui-cell__bd">
                     <input id="password" class="weui-input" type="password" placeholder="请输入密码">
                 </div>
             </div>
     </div>
-	<div class="weui-btn-area">
+    <div class="weui-btn-area">
             <a class="weui-btn weui-btn_primary" href="javascript:" id="submit">确定</a>
     </div>
-	<script type="text/javascript" src="https://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
+    <script type="text/javascript" src="https://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
     <script src="https://res.wx.qq.com/open/libs/weuijs/1.0.0/weui.min.js"></script>
     <script src="https://www.juntengshoes.cn/html/zepto.min.js"></script>
-	<script src="https://www.juntengshoes.cn/html/my.js"></script>
+    <script src="https://www.juntengshoes.cn/html/my.js"></script>
 </body>
 `
 
@@ -60,11 +60,24 @@ const PHOTO = `<!DOCTYPE html>
             <a class="weui-btn weui-btn_primary" href="javascript:" id="takephoto">选择图片</a>
             <a class="weui-btn weui-btn_primary" href="javascript:" id="upload">上传图片</a>
         </div>
+        <div class="js_dialog" id="confirmDialog" style="opacity: 0;">
+            <div class="weui-mask"></div>
+            <div class="weui-dialog weui-skin_android">
+                <div class="weui-dialog__hd"><strong class="weui-dialog__title">确认上传</strong></div>
+                <div class="weui-dialog__bd">
+                    已经整改完成，确定上传图片。
+                </div>
+                <div class="weui-dialog__ft">
+                    <a href="javascript:;" class="weui-dialog__btn weui-dialog__btn_default" id="cancel">取消</a>
+                    <a href="javascript:;" class="weui-dialog__btn weui-dialog__btn_primary" id="uploadConfirm">确定</a>
+                </div>
+            </div>
+        </div>
     </div>
-	<script type="text/javascript" src="https://res.wx.qq.com/open/js/jweixin-1.2.0.js"></script>
+    <script type="text/javascript" src="https://res.wx.qq.com/open/js/jweixin-1.2.0.js"></script>
     <script src="https://res.wx.qq.com/open/libs/weuijs/1.0.0/weui.min.js"></script>
     <script src="https://www.juntengshoes.cn/html/zepto.min.js"></script>
-	<script src="https://www.juntengshoes.cn/html/my.js"></script>
+    <script src="https://www.juntengshoes.cn/html/my.js"></script>
     <script type="text/javascript">
         if(!wx){//验证是否存在微信的js组件
             alert("微信接口调用失败，请检查是否引入微信js！");
@@ -86,7 +99,10 @@ const PHOTO = `<!DOCTYPE html>
         });
 
         $(function(){
-            $("#takephoto").click(function(){
+            var $takephoto = $("#takephoto");
+
+            $takephoto.click(function(){
+                alert('staring to take photo...');
                 wx.chooseImage({
                     count: 1,
                     sizeType: ['original', 'compressed'], 
@@ -98,8 +114,23 @@ const PHOTO = `<!DOCTYPE html>
                     }
                 });
             });
-        
-            $("#upload").click(function(){
+
+            $("#previewImg").click(function(){
+                alert('this src ...'+this.src);
+                wx.previewImage({
+                    current: this.src, // 当前显示图片的http链接
+                    urls: [this.src] // 需要预览的图片http链接列表
+                })
+            });
+
+            initUploadDialog();
+        });
+
+        function initUploadDialog(){
+            var $confirmDialog = $('#confirmDialog');
+
+            $("#uploadConfirm").click(function(){
+                
                 wx.uploadImage({
                     localId: _global_uploadImageId, // 需要上传的图片的本地ID，由chooseImage接口获得
                     isShowProgressTips: 1, // 默认为1，显示进度提示
@@ -114,17 +145,19 @@ const PHOTO = `<!DOCTYPE html>
                             console.log(data);
                             });
                         alert("upload success");
+                        $confirmDialog.fadeOut(200);
                     }
                 });
             });
 
-            $("#previewImg").click(function(){
-                wx.previewImage({
-                    current: this.src, // 当前显示图片的http链接
-                    urls: [] // 需要预览的图片http链接列表
-                })
-            })
-        });
+            $('#upload').on('click', function(){
+                $confirmDialog.fadeIn(200);
+            });
+
+            $('#cancel').on('click', function(){
+                $confirmDialog.fadeOut(200);
+            });
+        }
     </script>
 </body>
 `
